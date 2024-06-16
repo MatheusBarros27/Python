bf49e8a01a12df1e20804f2e7fc396869358db14
MatheusBarros27 <matheus.s.barros27@outlook.com>
2024-06-16 03:24:41 -0300

    Estruturas de Repetição

---
+++ EstruturasRepeticao.py
@@ -0,0 +1,43 @@
+#ESTRUTURAS DE REPETIÇÃO
+
+'''
+While [CONDIÇÃO]:       (Cuidado com laços infinitos)
+    [codigo while]
+
+For i=0 in Range(10):   (Usar Range(Numero) para gerar uma lista com n Valores que quer repetir ou atribuir a uma lista)
+    [codigo for]
+
+OBS. FOR: o Range(Numero) vai até (Numero - 1)
+     EX: Range(10) - Vai de 0 até 9
+         Range(1, 10) - Vai de 1 até 9
+         Range(1, 10, 2) - Vai de 1 a 9, pulando de 2 em 2. PASSO = 2
+'''
+
+#Exemplo 1: Acertar um número aleatório usando While
+import random
+
+print("Tente acertar o número sorteado!")
+
+numero_sorteado = random.randint(1, 20)
+print(numero_sorteado)
+numero_escolhido = int(input('Digite um número entre 1 a 20:'))
+print("")
+
+while numero_escolhido != numero_sorteado:
+    print("VOCÊ ERROU, TENTE NOVAMENTE!")
+    numero_escolhido = int(input('Digite um número entre 1 a 20:'))
+    print("")
+
+#exemplo 2: Imprimir o parabens 10 vezes Usando For
+
+i=1
+for i in range(10):
+    print(i, "-","PARABÉNS, VOCÊ ACERTOU!")
+    print("O número sorteado era o", numero_sorteado)
+    print("")
+
+
+#Comando para executar Clicando no arquivo
+input("Pressione Enter para fechar...")
+
+    
