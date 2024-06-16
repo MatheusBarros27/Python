30f8f24bf3deac6252620bbbd5c182ac150f157c
MatheusBarros27 <matheus.s.barros27@outlook.com>
2024-06-15 23:54:34 -0300

    "Variaveis e Comentários"

--- Primeiro.py
+++ Primeiro.py
@@ -1,4 +1,13 @@
+#Primeiro comentário
+
+#função imprimir na tela
 print("Hello World!")
 print("Seja bem vindo MATHEUS")
 
+#função receber um valor
 input("Digite PYTHON:")
+
+'''
+Vários comentarios 123
+
+'''
---
+++ Variaveis.py
@@ -0,0 +1,23 @@
+#VARIÁVEIS
+
+Nome = "MATHEUS"
+Idade = 24
+Altura = 1.68
+Estudando = True
+
+print("Olá ", Nome)
+print("Sua idade é:", Idade)
+print("Sua altura é:", Altura)
+print("Situação de Estudo = ", Estudando)
+
+Bairro = input("Você mora em qual bairro? ")
+
+print("Você mora no bairro:", Bairro)
+
+'''
+Int - Numeros inteiros
+Float - Numeros reais
+str - String (Texto)
+bool - Valores lógicos (TRUE e FALSE)
+
+'''
