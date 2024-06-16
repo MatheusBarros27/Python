a0ecda1485d1fd1933077cb4fa9c98be3b71a078
MatheusBarros27 <matheus.s.barros27@outlook.com>
2024-06-16 01:54:51 -0300

    Atualização Estrutura de repetição

--- EstruturasCondicionais.py
+++ EstruturasCondicionais.py
@@ -6,8 +6,10 @@ Estrutura IF
 
 if [CONDIÇÃO]:
     [comandos]
-else
-    [comandos Else]
+elif [CONDIÇÃO2]:
+    [comandos Else if ou elif]
+else:
+    [comandos else]
 
 OBS.:Não precisa abrir e fechar o comando
     pois o python utiliza a identação 
@@ -16,7 +18,7 @@ OBS.:Não precisa abrir e fechar o comando
 
 '''
 
-
+#Verificar Nome e Idade (Maior ou Menor de idade)
 print("Vamos verificar se você é maior de idade!")
 
 Nome = input("Digite seu Nome:")
@@ -27,6 +29,23 @@ if Idade >= 18:
 else:
     print(Nome, "Você Não é maior de idade!")
     
+
+#Verificar Situação das Notas (Aprovado, Recuperação ou Reprovado)
+print("Vamos agora verificar se você reprovou", Nome, "?")
+
+Nota1 = float(input("Digite sua primeira Nota:"))
+Nota2 = float(input("Digite sua segunda Nota:"))
+Media = (Nota1 + Nota2) / 2
+
+if Media >= 7:
+    print(Nome, "Você está APROVADO!")
+    print("PARABÉNS")
+elif Media >= 5:
+    print(Nome, "Você está de RECUPERAÇÃO!")
+    print("Não se preocupe você ainda tem mais uma chance, ESTUDE!")
+else:
+    print(Nome, "Você está REPROVADO!")
+    print("Só ano que vem agora! :( ")
 
 #Comando para executar Clicando no arquivo
 input("Pressione Enter para fechar...")
